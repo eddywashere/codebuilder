8099d3334bcf61b7a2fb690d5ea56654525470b9
Eddy Hernandez <edward.d.hernandez@gmail.com>
2017-06-18 17:22:02 -0700

    prettier all the things

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const steps = require('./lib/steps');
 
 exports.handler = (event, context) => {
-  const {step_name} = event;
+  const { step_name } = event;
   if (!step_name) {
     return steps['start_build'](event, context);
   } else if (!steps[step_name]) {
-    console.error('----- event debug -----', event, {step_name})
+    console.error('----- event debug -----', event, { step_name });
     return context.fail(`Error: Missing step function: ${step_name}`);
   }
 
